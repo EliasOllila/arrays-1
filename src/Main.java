9248fd121966b1c451367f5006d55c1d14f7c857
eliasollila <elias.ollila@syk.fi>
2022-09-05 18:36:33 +0300

    jotai

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //hw1
+        int values[]={2,5,-3,11,193};
+        for(int v : values)
+            System.out.println(v);
+        int sum=0;
+        for(int i=0; i<values.length; i++)
+        {
+            sum=sum+values[i];
+        }
+        System.out.println(sum);
+
+        //hw3
+        int[] values={-3, 2, 5, 4, 7};
+        int N=1;
+        boolean RESULT=true;
+
+        while(RESULT=true && N<5)
+        {
+            if(values[N]<values[N-1])
+                RESULT=false;
+            N++;
+        }
+        System.out.println(RESULT);
+
+
     }
 }
